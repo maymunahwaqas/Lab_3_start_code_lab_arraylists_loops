465c1a5a8e2ff471ef8e7b1465e4108eedd1dc54
Maymunah Waqas Butt <maymunahwaqas@outlook.com>
2023-07-10 17:19:35 +0100

    finished mvp

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -8,21 +9,51 @@ public class Runner {
 
 //        SCOTTISH ISLANDS
         List<String> scottishIslands = new ArrayList<>();
+        scottishIslands.add("Tiree");
         scottishIslands.add("Jura");
+        scottishIslands.add("Islay");
         scottishIslands.add("Mull");
         scottishIslands.add("Skye");
         scottishIslands.add("Arran");
         scottishIslands.add("Tresco");
+        scottishIslands.add("Coll");
 
-//        1. Add "Coll" to the end of the list
-//        2. Add "Tiree" to the start of the list
-//        3. Add "Islay" after "Jura" and before "Mull"
-//        4. Print out the index position of "Skye"
-//        5. Remove "Tresco" from the list by name
-//        6. Remove "Arran" from the list by index
-//        7. Print the number of islands in your arraylist
-//        8. Sort the list alphabetically
-//        9. Print out all the islands using a for loop
+
+        System.out.println(scottishIslands.indexOf("Skye"));
+        scottishIslands.remove("Tresco");
+
+        System.out.println(scottishIslands);
+        scottishIslands.remove(scottishIslands.indexOf("Arran"));
+
+        String firstscottishIslands = scottishIslands.get(0);
+        System.out.println("first scottish Islands:" + firstscottishIslands);
+        System.out.println("number of scottish Islands:" + scottishIslands.size() );
+
+        Collections.sort(scottishIslands, String.CASE_INSENSITIVE_ORDER);
+
+
+        ArrayList<String> allIslands = new ArrayList<>();
+        for (String allIsland : allIslands){
+            }
+        System.out.println(scottishIslands);
+
+
+
+
+
+
+
+
+
+//        1. Add "Coll" to the end of the list DONE
+//        2. Add "Tiree" to the start of the list DONE
+//        3. Add "Islay" after "Jura" and before "Mull" DONE
+//        4. Print out the index position of "Skye" DONE
+//        5. Remove "Tresco" from the list by name DONE
+//        6. Remove "Arran" from the list by index DONE
+//        7. Print the number of islands in your arraylist DONE
+//        8. Sort the list alphabetically DONE
+//        9. Print out all the islands using a for loop DONE
 
         System.out.println(scottishIslands);
 
